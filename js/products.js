ceecc82be0a15b13bffcde0a732331dbdff9f685
logachan <83635844+logachan@users.noreply.github.com>
2022-10-02 19:58:40 +0530

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,46 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Tinea Versicolor Antifungal Cream NATURAL Tinea Treatment Cream 100%FAST RELIEF",
+    desc : "Tinea Versicolor. Tinea Pedia.Cruris.Capitis.corporis ?. Pain & Suffering from Tinea Issues FOREVER. DESTROY Tinea Germs. - TineaBLAST kills Tinea causing GermsPermanently,thereby eliminating ALL future problems.",
+    img : "item1.jpeg",
+    price : 18.95
+  },
+  124: {
+    name : "Ringworm Cream Ringworm Treatment EXTRA STRENGTH FAST ACTING 100% SAFE LARGE",
+    desc : "Ringworm Driving you Crazy?. Ringworm PERMANENTLY ! RingwormBLAST will change your life and leave your skin happier, healthier, and Ringworm FREE! - Plant Based Proprietary Blend- Works WITH Immune System.",
+    img : "item2.jpeg",
+    price : 17.95
+  },
+  125: {
+    name : "Jock Itch Cream Tea Tree Antifungal EXTRA Strength Formula Herbal Medicated",
+    desc : "Jock Itch?.Pain & Suffering from Jock Itch IssuesFOREVER.DESTROY Jock Itch Germs.-JockitchBLAST kills Jock Itch causing GermsPermanently,thereby eliminating ALL futureproblems. ",
+    img : "item3.jpeg",
+    price : 13.95
+  },
+  126: {
+    name : "Molluscum Contagiosum Treatment Cream LARGE 60 ml Kids Adults 100% SAFE &NATURAL",
+    desc : "(MolluscumBLAST is 100% Natural. WITH Immune System, for Natural Healing. 15 Essential Oils ). MolluscumBLAST will change your life and leave your skin happier, healthier, and blister / outbreak FREE! ",
+    img : "item4.jpeg",
+    price : 18.95
+  },
+  127: {
+    name : "Shingles Relief Cream Treatment Natural Herbal Skincare #1 Selling Cream Online",
+    desc : "Pain & Suffering from Shingles Issues FOREVER. ShinglesBLAST will change your life and leave you happier, and Shingles Issues FREE!-0Harmful Chemicals. ShinglesBLAST gets to the root of the problem, rather than just clearing rashes.",
+    img : "item5.jpeg",
+    price : 18.95
+  },
+  128: {
+    name : "Scabies Treatment 2oz ScabiBLAST Cream  Medication Natural Permanent Relief FAST",
+    desc : "ScabiBLAST kills Scabies Mites and also their eggs, thereby eliminating ALL future problems. - ScabiBLAST is 100% Natural. ScabiBLAST gets to the root of the problem, rather than just clearing rashes.",
+    img : "item6.jpeg",
+    price : 19.95
+  },
+  129: {
+    name : "Herpes Treatment Cream Lips Genital HSV1 HSV2 HerpeBLAST SUPER BEST Suppression",
+    desc : "YOUR WAY with HerpeBLAST. - 1 HerpeBLAST Jar lasts for months. - HerpeBLAST is 100% Natural. HerpeBLAST will change your life and leave your skin happier, healthier, and outbreak FREE! Why not give HerpeBLAST a try?.",
+    img : "item7.jpeg",
+    price : 19.96
+  },
+  
+};
